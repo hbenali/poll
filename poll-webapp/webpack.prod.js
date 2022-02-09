46158423d50d998f96fe0fc8c61cefc432aaa831
Ayoub Zayati <ayoub.ezzayati@exoplatform.com>
2022-02-09 14:47:41 +0100

    Task-54102: Initialize poll addon project (#1)

---
+++ poll-webapp/webpack.prod.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+const config = {
+  context: path.resolve(__dirname, '.'),
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+          'eslint-loader',
+        ]
+      },
+      {
+        test: /\.vue$/,
+        use: [
+          'vue-loader',
+          'eslint-loader',
+        ]
+      }
+    ]
+  },
+  entry: {
+    poll: './src/main/webapp/vue-app/poll/main.js',
+  },
+  output: {
+    path: path.join(__dirname, 'target/poll/'),
+    filename: 'js/[name].bundle.js',
+    libraryTarget: 'amd'
+  },
+  externals: {
+    vue: 'Vue',
+    vuetify: 'Vuetify',
+    jquery: '$',
+  },
+};
+
+module.exports = config;
